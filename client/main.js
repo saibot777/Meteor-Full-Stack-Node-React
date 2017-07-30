193eca8440701c5514cdee27790c7345634e97e4
saibot777 <stefantrajkovic777@gmail.com>
2017-07-30 12:52:50 +0200

    rendering from mongo

--- client/main.js
+++ client/main.js
@@ -2,25 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+import {Players} from './../imports/api/players';
 import './main.html';
-
-const players = [
-  {
-    _id: '1',
-    name: 'Sladjana',
-    score: 99,
-  },
-  {
-    _id: '2',
-    name: 'Jovan',
-    score: -1,
-  },
-  {
-    _id: '3',
-    name: 'Stefan',
-    score: -12,
-  }
-];
 
 const renderPlayers = function(playersList) {
   return playersList.map(function (player) {
@@ -28,15 +11,18 @@ const renderPlayers = function(playersList) {
   });
 }
 
-Meteor.startup(function () {
-  let title = 'Score Keep';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      {[renderPlayers(players)]}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
+Meteor.startup(() => {
 
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        {[renderPlayers(players)]}
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
+});
 
